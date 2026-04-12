ab5ea919e8a87ada4180b7d4e7d28a124ce3f9a7
Silvana Escamilla <20253ds102@utez.edu.mx>
2026-04-12 00:49:52 -0600

    Se mejoro el archivo de paciente y se cambiio la estructura de main.fxml

--- Consultorio/src/main/java/com/example/consultorio/models/Paciente.java
+++ Consultorio/src/main/java/com/example/consultorio/models/Paciente.java
@@ -1,65 +1,56 @@
 package com.example.consultorio.models;
 
+
 public class Paciente {
     private String curp;
     private String nombre;
     private int edad;
-    private String tel;
+    private String telefono;
     private String alergias;
-    private String estatus; // ACTIVO o INACTIVO
+    private String estatus;
 
-    public Paciente(String curp, String nombre, int edad, String tel, String alergias, String estatus) {
+    public Paciente(String curp, String nombre, int edad, String telefono, String alergias, String estatus) {
         this.curp = curp;
         this.nombre = nombre;
         this.edad = edad;
-        this.tel = tel;
+        this.telefono = telefono;
         this.alergias = alergias;
         this.estatus = estatus;
     }
 
-    // Getters y Setters (Necesarios para el TableView)
+    // Getters
     public String getCurp() {
         return curp;
     }
-    public void setCurp(String curp) {
-        this.curp = curp; }
     public String getNombre() {
         return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
     public int getEdad() {
         return edad;
     }
-    public void setEdad(int edad) {
-        this.edad = edad;
+    public String getTelefono() {
+        return telefono;
     }
-    public String getTel() {
-        return tel;
-    }
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
-    public String getAlergias() {
-        return alergias;
-    }
-    public void setAlergias(String alergias) {
-        this.alergias = alergias;
+    public String getAlergias() { return alergias;
     }
     public String getEstatus() {
         return estatus;
     }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public void setAlergias(String alergias) {
+        this.alergias = alergias;
+    }
     public void setEstatus(String estatus) {
         this.estatus = estatus;
-    }
-
-    @Override
-    public String toString() {
-
-    }
-
-    public String toCSV() {
-
     }
 }
